e112343e92964524ff9fa84de88432e177de4961
sig6774@naver.com <sig6774@naver.com>
2025-02-16 16:53:30 +0900

    20250216

--- DFS_Basic/부분집합구하기(DFS).py
+++ DFS_Basic/부분집합구하기(DFS).py
@@ -18,3 +18,22 @@
 2
 3
 """
+
+def DFS(v):
+    if v==n+1:
+        for i in range(1, n+1):
+            if ch[i]==1:
+                print(i, end=' ')
+        print()
+    else:
+        ch[v]=1
+        DFS(v+1)
+        ch[v]=0
+        DFS(v+1)
+
+if __name__ == "__main__":
+
+    n=3
+    ch=[0]*(n+1)
+    DFS(1)
+    
