6e0adeda6214cbb48d7e63567db50c022e5aad5a
sig6774@naver.com <sig6774@naver.com>
2026-04-06 22:04:02 +0900

    20260406

---
+++ SQL/5월_식품들의_총매출_조회하기.sql
@@ -0,0 +1,38 @@
+/*
+문제 설명
+다음은 식품의 정보를 담은 FOOD_PRODUCT 테이블과 식품의 주문 정보를 담은 FOOD_ORDER 테이블입니다. FOOD_PRODUCT 테이블은 다음과 같으며 PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, PRICE는 식품 ID, 식품 이름, 식품코드, 식품분류, 식품 가격을 의미합니다.
+
+Column name	Type	Nullable
+PRODUCT_ID	VARCHAR(10)	FALSE
+PRODUCT_NAME	VARCHAR(50)	FALSE
+PRODUCT_CD	VARCHAR(10)	TRUE
+CATEGORY	VARCHAR(10)	TRUE
+PRICE	NUMBER	TRUE
+FOOD_ORDER 테이블은 다음과 같으며 ORDER_ID, PRODUCT_ID, AMOUNT, PRODUCE_DATE, IN_DATE, OUT_DATE, FACTORY_ID, WAREHOUSE_ID는 각각 주문 ID, 제품 ID, 주문량, 생산일자, 입고일자, 출고일자, 공장 ID, 창고 ID를 의미합니다.
+
+Column name	Type	Nullable
+ORDER_ID	VARCHAR(10)	FALSE
+PRODUCT_ID	VARCHAR(5)	FALSE
+AMOUNT	NUMBER	FALSE
+PRODUCE_DATE	DATE	TRUE
+IN_DATE	DATE	TRUE
+OUT_DATE	DATE	TRUE
+FACTORY_ID	VARCHAR(10)	FALSE
+WAREHOUSE_ID	VARCHAR(10)	FALSE
+문제
+FOOD_PRODUCT와 FOOD_ORDER 테이블에서 생산일자가 2022년 5월인 식품들의 식품 ID, 식품 이름, 총매출을 조회하는 SQL문을 작성해주세요. 이때 결과는 총매출을 기준으로 내림차순 정렬해주시고 총매출이 같다면 식품 ID를 기준으로 오름차순 정렬해주세요.
+*/
+
+WITH Q as (SELECT b.PRODUCT_ID, a.PRODUCT_NAME, SUM(a.price * b.amount) as TOTAL_SALES 
+FROM FOOD_PRODUCT a 
+JOIN FOOD_ORDER b 
+ON a.PRODUCT_ID = b.PRODUCT_ID 
+WHERE date_format(b.PRODUCE_DATE, '%Y-%m') = '2022-05'
+GROUP BY b.PRODUCT_ID, a.PRODUCT_NAME)
+
+SELECT Q.*
+FROM Q 
+order by Q.TOTAL_SALES DESC, Q.PRODUCT_ID asc
+;
+
+
