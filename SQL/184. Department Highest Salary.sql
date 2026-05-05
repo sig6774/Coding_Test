30191e37dedc855a76dd5e3996472b03b132c74e
sig6774@naver.com <sig6774@naver.com>
2026-05-05 23:02:20 +0900

    20260505

---
+++ SQL/184. Department Highest Salary.sql
@@ -0,0 +1,88 @@
+/*
+Table: Employee
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key (column with unique values) for this table.
+departmentId is a foreign key (reference columns) of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+ 
+
+Table: Department
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table. It is guaranteed that department name is not NULL.
+Each row of this table indicates the ID of a department and its name.
+ 
+
+Write a solution to find employees who have the highest salary in each of the departments.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+-------+--------+--------------+
+| id | name  | salary | departmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Jim   | 90000  | 1            |
+| 3  | Henry | 80000  | 2            |
+| 4  | Sam   | 60000  | 2            |
+| 5  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+Department table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+Output: 
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Jim      | 90000  |
+| Sales      | Henry    | 80000  |
+| IT         | Max      | 90000  |
++------------+----------+--------+
+Explanation: Max and Jim both have the highest salary in the IT department and Henry has the highest salary in the Sales department.
+*/
+
+/*
+1. employee table에서 departmentId 기준으로 max salary 추출 
+2. 추추한 테이블과 department 테이블을 id와 departmentId 기준으로 조인하여 최종 결과 추출
+*/
+
+with q as (
+    select departmentId, max(salary) as salary
+        from Employee 
+    group by departmentId 
+)
+select b.name as Department
+        , a.name as Employee 
+        , a.salary as Salary
+from Employee as a 
+join Department as b 
+on a.departmentId = b.id
+left join q as c 
+on a.departmentId = c.departmentId 
+where a.departmentId = c.departmentId 
+and a.salary = c.salary
+;
